9d6b85c43643e06af7edc6d25e251fbe6a64cb71
Алексей <akarpushev@yandex.ru>
2024-08-29 17:01:49 +0300

    admin.py

--- bikes/admin.py
+++ bikes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Bike
 
-# Register your models here.
+@admin.register(Bike)
+class BikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'bike', 'status')
+    search_fields = ('bike', 'status')
+    list_filter = ('status',)
+
+
+# admin.site.register(Bike)
