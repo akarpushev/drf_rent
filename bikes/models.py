0070a7891e41fee7d423ed8613c372002c87941e
Алексей <akarpushev@yandex.ru>
2024-08-31 22:22:22 +0300

    bikes.models

--- bikes/models.py
+++ bikes/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Bike(models.Model):
     bike = models.CharField(max_length=5)
     status = models.CharField(max_length=50, choices=[('available', 'Available'), ('rented', 'Rented')])
 
+
+class Rent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bike = models.OneToOneField(Bike, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
